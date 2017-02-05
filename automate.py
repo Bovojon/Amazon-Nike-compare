fc112bba08a87a44f2753151d6a4bf2b132b7ceb
bovojon <bovojon@gmail.com>
2017-02-05 00:55:24 -0500

    Use python's requests to get data from a link

--- automate.py
+++ automate.py
@@ -1,3 +1,10 @@
 import webbrowser
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(type(res))
+print(res.status_code == requests.codes.ok)
+print(len(res.text))
+print(res.text[:250])
 
 webbrowser.open('http://inventwithpython.com/')
