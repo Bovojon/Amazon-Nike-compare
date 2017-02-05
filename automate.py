88d342161b424f1564ca871164bf06063e1521b2
bovojon <bovojon@gmail.com>
2017-02-05 00:58:01 -0500

    Use res.raise_for_status() to check for errors

--- automate.py
+++ automate.py
@@ -3,7 +3,10 @@ import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 print(type(res))
-print(res.status_code == requests.codes.ok)
+
+# If there is an error in downloading the file, raise an error:
+res.raise_for_status()
+
 print(len(res.text))
 print(res.text[:250])
 
