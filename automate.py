d0286257a495facb27ccca7c18b05d4086d0bd19
bovojon <bovojon@gmail.com>
2017-02-05 01:06:29 -0500

    Write downloaded data to file passing wb to open() - to save unicode of text

--- automate.py
+++ automate.py
@@ -2,15 +2,9 @@ import webbrowser
 import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-print(type(res))
+res.raise_for_status()
 
-# If there is an error in downloading the file, raise an error:
-try:
-    res.raise_for_status()
-except Exception as exc:
-    print("There was a problem: %s" %(exc))
-
-print(len(res.text))
-print(res.text[:250])
-
-webbrowser.open('http://inventwithpython.com/')
+playFile = open("romeojuliet.text", "wb")
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+playFile.close()
