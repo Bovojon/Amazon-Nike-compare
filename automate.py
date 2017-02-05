9fdb83beec0509f272ea41055b7994a1b01aef77
bovojon <bovojon@gmail.com>
2017-02-05 01:02:22 -0500

    Use try-except

--- automate.py
+++ automate.py
@@ -5,7 +5,10 @@ res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
 print(type(res))
 
 # If there is an error in downloading the file, raise an error:
-res.raise_for_status()
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print("There was a problem: %s" %(exc))
 
 print(len(res.text))
 print(res.text[:250])
