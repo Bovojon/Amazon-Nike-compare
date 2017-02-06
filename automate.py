b03ab79fad479f40aa01bb007ea5f83b9a7a5281
bovojon <bovojon@gmail.com>
2017-02-06 14:05:42 -0500

    Scrap automatetheboringstuff.com and get element css book

--- automate.py
+++ automate.py
@@ -3,11 +3,20 @@ import requests
 import bs4
 
 
-res = requests.get('http://nostarch.com')
+res = requests.get('https://automatetheboringstuff.com/chapter11/')
 res.raise_for_status()
+fileName = open(res.text)
 # The bs4.BeautifulSoup() function returns is a BeautifulSoup object.
-noStarchSoup = bs4.BeautifulSoup(res.text, "lxml")
-print(type(noStarchSoup))
+noStarchSoup = bs4.BeautifulSoup(fileName.read(), "lxml")
+elems = noStarchSoup.select(".book")
+print(type(elems))
+print(len(elems))
+print(type(elems[0]))
+elems[0].getText()
+str(elems[0])
+elems[0].attrs
+
+#print(type(noStarchSoup))
 
 
 # playFile = open("romeojuliet.text", "wb")
