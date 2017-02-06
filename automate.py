51bb6b983251bf084308aaee09e7616c83425e7c
bovojon <bovojon@gmail.com>
2017-02-06 13:57:41 -0500

    Use beautifulSoup4 to parse HTML from noStarch.com

--- automate.py
+++ automate.py
@@ -1,10 +1,16 @@
 import webbrowser
 import requests
+import bs4
 
-res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+
+res = requests.get('http://nostarch.com')
 res.raise_for_status()
+# The bs4.BeautifulSoup() function returns is a BeautifulSoup object.
+noStarchSoup = bs4.BeautifulSoup(res.text, "lxml")
+print(type(noStarchSoup))
 
-playFile = open("romeojuliet.text", "wb")
-for chunk in res.iter_content(100000):
-    playFile.write(chunk)
-playFile.close()
+
+# playFile = open("romeojuliet.text", "wb")
+# for chunk in res.iter_content(100000):
+#     playFile.write(chunk)
+# playFile.close()
