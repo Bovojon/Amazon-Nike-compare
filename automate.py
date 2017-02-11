eb6fab6ac198c3d5c1ae7cf220b651875e32209b
bovojon <bovojon@gmail.com>
2017-02-11 01:23:59 -0500

    Search Amazon using key words and save results html page

--- automate.py
+++ automate.py
@@ -1,14 +1,35 @@
-import webbrowser
-import requests
-import bs4
+#! python3
+# automate.py - Searched Amazon and Nike and compares prices
+import webbrowser, requests, bs4
 
-exampleFile = open('example.html')
-exampleSoup = bs4.BeautifulSoup(exampleFile.read(), "lxml")
 
-elems = exampleSoup.select("#author")
-print(type(elems))
-print(len(elems))
-print(type(elems[0]))
-print(elems[0].getText())
-print(str(elems[0]))
-print(elems[0].attrs)
+print('Loading...')
+
+search = raw_input("Please enter a search: ")
+search = search.lower()
+search = search.split(" ")
+search = search[0]+"+"+search[1]
+
+with open('data.html', 'wb') as f:
+    response = requests.get('https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + ' '+search, headers={
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
+    }, stream=True)
+    response.raise_for_status()
+
+    if not response.ok:
+        print("There was an error")
+
+    for block in response.iter_content(1024):
+        f.write(block)
+
+
+# Retrieve top search result links.
+soup = bs4.BeautifulSoup(open("data.html"), "lxml")
+# Open a browser tab for each result.
+linkElems = soup.select('.r a')
+print(linkElems)
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+
+f.close()
