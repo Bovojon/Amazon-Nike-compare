2b386aa6c0630969eb1cb4e8d10baafb91d04b0f
bovojon <bovojon@gmail.com>
2017-02-11 01:52:25 -0500

    Search Amazon and Nike with only 2 keywords

--- automate.py
+++ automate.py
@@ -2,34 +2,43 @@
 # automate.py - Searched Amazon and Nike and compares prices
 import webbrowser, requests, bs4
 
-
-print('Loading...')
-
-search = raw_input("Please enter a search: ")
-search = search.lower()
-search = search.split(" ")
-search = search[0]+"+"+search[1]
-
-with open('data.html', 'wb') as f:
-    response = requests.get('https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=' + ' '+search, headers={
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
-    }, stream=True)
-    response.raise_for_status()
-
-    if not response.ok:
-        print("There was an error")
-
-    for block in response.iter_content(1024):
-        f.write(block)
+def get_keywords():
+    search = raw_input("Please enter a search: ")
+    search = search.lower()
+    search = search.split(" ")
+    return search
 
 
-# Retrieve top search result links.
-soup = bs4.BeautifulSoup(open("data.html"), "lxml")
-# Open a browser tab for each result.
-linkElems = soup.select('.r a')
-print(linkElems)
-numOpen = min(5, len(linkElems))
-for i in range(numOpen):
-    webbrowser.open('http://google.com' + linkElems[i].get('href'))
+def get_data():
+    search = get_keywords()
 
-f.close()
+    # Search Amazon
+    search_amazon = search[0]+"+"+search[1]
+    with open('amazon.html', 'wb') as amazon:
+        response = requests.get('https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=nike' + ' '+search_amazon, headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
+        }, stream=True)
+        response.raise_for_status()
+        if not response.ok:
+            print("There was an error")
+        for block in response.iter_content(1024):
+            amazon.write(block)
+
+    # Search Nike
+    search_nike = search[0]+"%20"+search[1]
+    with open('nike.html', 'wb') as nike:
+        response = requests.get('http://store.nike.com/us/en_us/pw/n/1j7?sl=' + ' '+search_nike, headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
+        }, stream=True)
+        response.raise_for_status()
+        if not response.ok:
+            print("There was an error")
+        for block in response.iter_content(1024):
+            nike.write(block)
+
+    # Close files
+    amazon.close()
+    nike.close()
+
+if __name__ == '__main__':
+    get_data()
